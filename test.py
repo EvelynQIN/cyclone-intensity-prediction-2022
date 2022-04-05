745e02d4a959ff03b9323392319b661f2554c6f8
Feichi Lu <171840094caroline@gmail.com>
2022-04-05 20:14:38 +0200

    add_moving_average

--- test.py
+++ test.py
@@ -1,5 +1,7 @@
 
 from dataset import extract_timeseries
+from moving_average import moving_average
+from sklearn.metrics import mean_squared_error
 
 
 if __name__ == '__main__':
@@ -21,6 +23,10 @@ if __name__ == '__main__':
     print('Shape of labels: {}'.format(labels.shape))
 
     print('Shape of moving_avg: {}'.format(moving_avg.shape))
+
+    pred_intensity = moving_average(meta_features)
+    true_intensity = labels.reshape(-1, labels.shape[1])
+    mean_squared_error(pred_intensity, true_intensity)
 
 
 
